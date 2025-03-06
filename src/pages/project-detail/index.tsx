3ec20df3ade63a9615ff3c168c3de8b1dfbb6998
Tejas <tejassharma2021@outlook.com>
2025-03-06 17:24:56 +0530

    FIX: protected pages

--- src/pages/project-detail/index.tsx
+++ src/pages/project-detail/index.tsx
@@ -1,11 +1,23 @@
+import { Navigate } from "react-router-dom";
+
 import GlobalLayout from "@/components/layout/global-layout";
 
+import useAuthStore from "@/models/auth";
+
+import { ROUTE_MAP } from "@/data/routes";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Board from "./components/board";
 
 export default function ProjectDetail() {
+  const { isAuth } = useAuthStore();
+
+  // TODO: Need to create a global component then wrap this login inside
+  if (!isAuth) {
+    return <Navigate to={ROUTE_MAP.AUTH.LOGIN} replace />;
+  }
+
   return (
     <GlobalLayout>
       <DndProvider backend={HTML5Backend}>
